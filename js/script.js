bd3e763a82e6ba05884def9f3698f48c0e6c1c8c
Tofunmi Kusimo <tofunmikusimo21@gmail.com>
2023-05-03 10:08:13 +0100

    move script.js to js folder

--- js/script.js
+++ js/script.js
@@ -8,48 +8,22 @@ $(document).ready(function() {
         const gender = $("select#gender").val();
     
 
-        if ( racism === 'Yes' && bias === 'No'
-            && trump === 'No' 
-            && border === 'No'
-            && sports === 'Yes' 
-            && gender === 'Yes') {
+        if ( racism === 'Yes' && bias === 'No' && trump === 'No' && border === 'No' && sports === 'Yes' && gender === 'Yes') {
             $(".formOne").hide();
             $(".liberal").show();
-        } else if ( racism === 'No'
-            && bias === 'No' 
-            && trump === 'Yes' 
-            && border === 'Yes' 
-            && sports === 'No' 
-            && gender === 'Not all') {
+        } else if ( racism === 'No' && bias === 'No' && trump === 'Yes' && border === 'Yes' && sports === 'No' && gender === 'Not all') {
             $(".formOne").hide();
             $(".moderate").show();
-        }  else if ( racism === 'Yes' 
-        && bias === 'No' 
-        && trump === 'No' 
-        && border === 'Yes' 
-        && sports === 'No' 
-        && gender === 'Not all') {
+        }  else if ( racism === 'Yes' && bias === 'No' && trump === 'No' && border === 'Yes' && sports === 'No' && gender === 'Not all') {
             $(".formOne").hide();
             $(".liberal").show();
-        }  else if ( racism === 'Yes' 
-        && bias === 'No' 
-        && trump === 'No' 
-        && border === 'Yes' 
-        && sports === 'No' 
-        && gender === 'No') {
+        }  else if ( racism === 'Yes' && bias === 'No' && trump === 'No' && border === 'Yes' && sports === 'No' && gender === 'No') {
             $(".formOne").hide();
             $(".liberal").show();
-        } else if ( racism === 'No' 
-            && bias === 'No' 
-            && trump === 'No' 
-            && border === 'Yes' 
-            && sports === 'Yes' 
-            && gender === 'Not all') {
+        } else if ( racism === 'No' && bias === 'No' && trump === 'No' && border === 'Yes' && sports === 'Yes' && gender === 'Not all') {
             $(".formOne").hide();
             $(".liberal").show();
-        } else if (racism === 'No' && bias === 'Yes' 
-            && trump === 'Yes' && border === 'Yes'
-            && sports === 'No' && gender === 'No') {
+        } else if (racism === 'No' && bias === 'Yes' && trump === 'Yes' && border === 'Yes' && sports === 'No' && gender === 'No') {
             $(".formOne").hide();
             $(".conservative").show();
         } else {
